e1936368499cc246906b7eddede4229d8e32c64f
PirateNahid <nhdnahid2022@gmail.com>
2022-08-30 09:40:23 +0600

    v

--- app/src/main/java/com/piratenahid/watandroid/MainActivity.java
+++ app/src/main/java/com/piratenahid/watandroid/MainActivity.java
@@ -1,18 +1,51 @@
 package com.piratenahid.watandroid;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    List<String> wordList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("words");
+
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    String word = dataSnapshot.child("name").getValue(String.class);
+                    Log.d("TAG", word);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+            }
+        });
+
+
+
     }
 }

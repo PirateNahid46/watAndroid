b8dc39f3e3619f43b11cea1ab1f37cb6587a8d79
PirateNahid <piratenahid46@gmail.com>
2022-08-31 22:33:46 +0600

    vvvvv

--- app/src/main/java/com/piratenahid/watandroid/MainActivity.java
+++ app/src/main/java/com/piratenahid/watandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.piratenahid.watandroid;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +15,25 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     List<String> wordList;
+    Button start;
+    LinearLayout linearLayoutSettings, linearLayoutWat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wordList = new ArrayList<String>();
+        start = findViewById(R.id.start_button);
+        linearLayoutSettings = findViewById(R.id.settings_lin);
+        linearLayoutWat = findViewById(R.id.wat_lin);
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("words");
 
 
@@ -33,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
                 for(DataSnapshot dataSnapshot: snapshot.getChildren()){
                     String word = dataSnapshot.child("name").getValue(String.class);
-                    Log.d("TAG", word);
+                    wordList.add(word);
                 }
 
             }
@@ -44,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayoutSettings.setVisibility(View.GONE);
+                linearLayoutWat.setVisibility(View.VISIBLE);
+            }
+        });
+
+
 
 
 
